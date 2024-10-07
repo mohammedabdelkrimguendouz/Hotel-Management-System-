67c347da6113d0074b35c932a9c9b1de61cf8edd
unknown <mohammedabdelkrimguendouz@gmail.com>
2024-10-07 16:42:26 +0100

    publish commit

--- Version Back-End Server Side.(.net Core)/Hotel/obj/Debug/net8.0/Hotel.AssemblyInfo.cs
+++ Version Back-End Server Side.(.net Core)/Hotel/obj/Debug/net8.0/Hotel.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Hotel")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4586d192eb65d0746c998a580db8eaec6b1787eb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Hotel")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Hotel")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Version Back-End Server Side.(.net Core)/Hotel_DataAccess/obj/Debug/net8.0/Hotel_DataAccess.AssemblyInfo.cs
+++ Version Back-End Server Side.(.net Core)/Hotel_DataAccess/obj/Debug/net8.0/Hotel_DataAccess.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Hotel_DataAccess")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4586d192eb65d0746c998a580db8eaec6b1787eb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Hotel_DataAccess")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Hotel_DataAccess")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
